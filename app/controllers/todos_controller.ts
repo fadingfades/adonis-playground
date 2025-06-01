f6d759e2b8bb5b2193263fd37f83c449d11ab3cc
fadingfades <fadlyandriyanto9@gmail.com>
2025-06-02 02:35:18 +0800

    todo: Implement CRUD features
    - Use bootstrap for styling
    - Use modal for add todo
    
    Co-authored-by: favolia <daisszo1455@gmail.com>
    Signed-off-by: fadingfades <fadlyandriyanto9@gmail.com>

--- app/controllers/todos_controller.ts
+++ app/controllers/todos_controller.ts
@@ -1,4 +1,47 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import Todo from '#models/todo'
 
 export default class TodosController {
+    async index({ view }: HttpContext) {
+        const todos = await Todo.all()
+
+        return view.render('pages/todo', { todos })
+    }
+
+    async store({request, response}: HttpContext) {
+        const {name, description, status} = request.body()
+        await Todo.create({
+            name,
+            description,
+            status: !!status
+        })
+
+        return response.redirect().toRoute('todo.home')
+    }
+
+    async edit({ view, request }: HttpContext) {
+        const todoId = request.param('id')
+        const todo = await Todo.findOrFail(todoId)
+
+        return view.render('pages/edit_todo', { todo })
+    }
+
+    async update({ request, response }: HttpContext) {
+        const todoId = request.param('id')
+        const todo = await Todo.findOrFail(todoId)
+        const {name, description, status} = request.body()
+
+        await todo.merge({ name, description, status: !!status }).save()
+
+        return response.redirect().toRoute('todo.home')
+    }
+
+    async delete({ request, response }: HttpContext) {
+        const todoId = request.param('id')
+        const todo = await Todo.findOrFail(todoId)
+
+        await todo.delete()
+
+        return response.redirect().toRoute('todo.home')
+    }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,14 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import TodosController from '#controllers/todos_controller'
 
 router.on('/').render('pages/home')
+
+router.group(() => {
+    router.get('home', [TodosController, 'index']).as('todo.home')
+    router.post('/store', [TodosController, 'store']).as('todo.store')
+    router.get('/edit/:id', [TodosController, 'edit']).as('todo.edit')
+    router.post('/update/:id', [TodosController, 'update']).as('todo.update')
+    router.post('/delete/:id', [TodosController, 'delete']).as('todo.delete')
+}).prefix('/todo')
